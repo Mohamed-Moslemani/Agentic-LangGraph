9c4cd128ffb796fc3b9707f2f4a7f0fdb2f25016
Mohamed Moslemani <moslemanomohamed@gmail.com>
2025-11-06 14:46:36 +0200

    main change

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 
-from app.graph.build_graph import build_graph
-from app.memory.checkpoint import get_checkpointer
+from graph.build_graph import build_graph
+from memory.checkpoint import get_checkpointer
 
 def main():
     builder = build_graph()
@@ -8,10 +8,8 @@ def main():
 
     app = builder.compile(checkpointer=checkpointer)
 
-    # Example user chat
     config = {"configurable": {"thread_id": "user_123"}}
 
-    # User wants to change their PIN
     inputs = {
         "messages": [
             {"role": "user", "content": "I want to change the PIN of my card token XYZ for client 1001 to 1234"}
@@ -21,7 +19,7 @@ def main():
     for event in app.stream(inputs, config=config, stream_mode="values"):
         print("STEP:", event)
 
-    
+
 
 if __name__ == "__main__":
     main()
