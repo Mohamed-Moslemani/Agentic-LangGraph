a7a141e659f541a147af2db814a57855b1df533d
Mohamed Moslemani <moslemanomohamed@gmail.com>
2025-11-06 14:24:54 +0200

    context manager error fix

--- app/memory/checkpoint.py
+++ app/memory/checkpoint.py
@@ -3,4 +3,8 @@ from langgraph.checkpoint.redis import RedisSaver
 
 def get_checkpointer():
     redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-    return RedisSaver.from_conn_string(redis_url)
+
+    with RedisSaver.from_conn_string(redis_url) as checkpointer:
+        print(f"Connected to Redis at {redis_url}")
+        print(type(checkpointer))
+        return checkpointer
